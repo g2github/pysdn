b4f9d73da844e9d87053f2721fe730275b61cad2
G2 <g2sleeper@me.com>
2015-12-04 10:53:47 -0500

    Added vr5600 cfg for comcast poc.

--- samples/samplenetconf/demos/vr_cfg.py
+++ samples/samplenetconf/demos/vr_cfg.py
@@ -42,13 +42,11 @@
 import logging
 import argparse
 import sys
-import time
-import json
+
 
 from pysdn.controller.controller import Controller
 from pysdn.netconfdev.vrouter.vrouter5600 import VRouter5600
-from pysdn.netconfdev.vrouter.vrouter5600 import NetconfNode
-from pysdn.common.status import OperStatus, STATUS
+from pysdn.common.status import STATUS
 from pysdn.common.utils import load_dict_from_file
 from pysdn.netconfdev.vrouter.interfaces import DataPlaneInterface
 from pysdn.netconfdev.vrouter.protocols import Ospf
@@ -91,45 +89,79 @@ def read_in_vr_cfg_file():
 
     return d
 
-def set_ospf_param(ctrl, vr_cfg_params):
+def set_loopback_interface_1(ctrl, vr_cfg_params):
     templateUrl = ("http://{}:{}/restconf/config/"
                     "opendaylight-inventory:nodes/node/{}/"
                    "yang-ext:mount/"
-                   "vyatta-protocols:protocols/vyatta-protocols-ospf:ospf")
-    url = templateUrl.format(vr_cfg_params['ctrlIpAddr'], vr_cfg_params['ctrlPortNum'], vr_cfg_params['nodeName_1'])
-    var = '{{\"vyatta-protocols-ospf:ospf\": {{\"parameters\": {{\"router-id\": \"{}\"}}}}}}'
-    payload = var.format(vr_cfg_params['ospfParamRouterId_1'])
+                   "vyatta-interfaces:interfaces/vyatta-interfaces-loopback:loopback/{}")
+    url = templateUrl.format(vr_cfg_params['ctrlIpAddr'], vr_cfg_params['ctrlPortNum'], vr_cfg_params['nodeName_1'], vr_cfg_params['lpbkName_1'])
+
+#    voo = '{{\"vyatta-interfaces-loopback:loopback\": [{{\"tagnode\": \"{}\",\"address\": [\"{}\"]}}]}}'
+
+    var = '{{\"vyatta-interfaces-loopback:loopback\": [{{\"tagnode\": \"{}\",\"address\": [\"{}\"]}}]}}'
+    payload = var.format(vr_cfg_params['lpbkName_1'], vr_cfg_params['lpbkAddr_1'])
     headers = {"content-type": "application/json", "accept": "application/json"}
 
-    logging.debug(url)
-    logging.debug(payload)
+    logging.info(url)
+    logging.info(payload)
 
     resp = ctrl.http_put_request(url, payload, headers)
 
     logging.info(resp)
 
-def set_ospf_area(ctrl, vr_cfg_params):
+def set_loopback_interface_2(ctrl, vr_cfg_params):
     templateUrl = ("http://{}:{}/restconf/config/"
                     "opendaylight-inventory:nodes/node/{}/"
                    "yang-ext:mount/"
-                   "vyatta-protocols:protocols/vyatta-protocols-ospf:ospf")
-    url = templateUrl.format(vr_cfg_params['ctrlIpAddr'], vr_cfg_params['ctrlPortNum'], vr_cfg_params['nodeName_1'])
-    var = '{{\"vyatta-protocols-ospf:ospf\": {{\"parameters\": {{\"router-id\": \"{}\"}}}}}}'
-    payload = var.format(vr_cfg_params['ospfParamRouterId_1'])
+                   "vyatta-interfaces:interfaces/vyatta-interfaces-loopback:loopback/{}")
+    url = templateUrl.format(vr_cfg_params['ctrlIpAddr'], vr_cfg_params['ctrlPortNum'], vr_cfg_params['nodeName_1'], vr_cfg_params['lpbkName_1'])
+
+    var = '{{\"vyatta-interfaces-loopback:loopback\": [{{\"tagnode\": \"{}\",\"address\": [\"{}\"]}}]}}'
+    payload = var.format(vr_cfg_params['lpbkName_2'], vr_cfg_params['lpbkAddr_2'])
     headers = {"content-type": "application/json", "accept": "application/json"}
 
-    logging.debug(url)
-    logging.debug(payload)
+    logging.info(url)
+    logging.info(payload)
 
     resp = ctrl.http_put_request(url, payload, headers)
 
     logging.info(resp)
 
 
-#  set interfaces loopback lo address 10.0.0.11/32
-#  set interfaces dataplane dp0s3 address 192.168.21.1/24
-#  set interfaces dataplane dp0s6 address 192.168.11.1/24
-#  set interfaces dataplane dp0s? address 192.168.22.1/24
+def set_ospf_cfg_1(ctrl, vr_cfg_params):
+    templateUrl = ("http://{}:{}/restconf/config/"
+                    "opendaylight-inventory:nodes/node/{}/"
+                   "yang-ext:mount/"
+                   "vyatta-protocols:protocols/vyatta-protocols-ospf:ospf")
+    url = templateUrl.format(vr_cfg_params['ctrlIpAddr'], vr_cfg_params['ctrlPortNum'], vr_cfg_params['nodeName_1'])
+    var = '{{\"vyatta-protocols-ospf:ospf\": {{\"passive-interface\": [\"{}"],\"area\": [{{\"tagnode\": \"{}\",\"network\": [\"{}\"]}},{{\"tagnode\": \"{}\",\"network\": [\"{}\",\"{}\",\"{}\"]}}],\"parameters\": {{\"router-id\": \"{}\"}}}}}}'
+    payload = var.format(vr_cfg_params['ospfPsvIf_1'], vr_cfg_params['ospfArea4_1'], vr_cfg_params['ospfNetwork4_1'], vr_cfg_params['ospfArea3_1'], vr_cfg_params['ospfNetwork3_1'], vr_cfg_params['ospfNetwork2_1'], vr_cfg_params['ospfNetwork1_1'], vr_cfg_params['ospfParamRouterId_1'],)
+    headers = {"content-type": "application/json", "accept": "application/json"}
+
+    logging.info(url)
+    logging.info(payload)
+
+    resp = ctrl.http_put_request(url, payload, headers)
+
+    logging.info(resp)
+
+def set_ospf_cfg_2(ctrl, vr_cfg_params):
+    templateUrl = ("http://{}:{}/restconf/config/"
+                    "opendaylight-inventory:nodes/node/{}/"
+                   "yang-ext:mount/"
+                   "vyatta-protocols:protocols/vyatta-protocols-ospf:ospf")
+    url = templateUrl.format(vr_cfg_params['ctrlIpAddr'], vr_cfg_params['ctrlPortNum'], vr_cfg_params['nodeName_2'])
+
+    var = '{{\"vyatta-protocols-ospf:ospf\": {{\"passive-interface\": [\"{}\"],\"area\": [{{\"tagnode\": \"{}\",\"network\": [\"{}\"]}},{{\"tagnode\": \"{}\",\"network\": [\"{}\",\"{}\"]}}],\"parameters\": {{\"router-id\": \"10.0.0.12\"}}}}}}'
+    payload = var.format(vr_cfg_params['ospfPsvIf_2'], vr_cfg_params['ospfArea3_2'], vr_cfg_params['ospfNetwork3_2'], vr_cfg_params['ospfNetwork2_2'], vr_cfg_params['ospfArea1_2'], vr_cfg_params['ospfNetwork1_2'], vr_cfg_params['ospfNetwork2_2'], vr_cfg_params['ospfParamRouterId_2'])
+    headers = {"content-type": "application/json", "accept": "application/json"}
+
+    logging.info(url)
+    logging.info(payload)
+
+    resp = ctrl.http_put_request(url, payload, headers)
+
+    logging.info(resp)
 
 
 def do_vr_cfg_1(vr_cfg_params):
@@ -144,42 +176,41 @@ def do_vr_cfg_1(vr_cfg_params):
     node_configured = False
     result = ctrl.check_node_config_status(vr_cfg_params['nodeName_1'])
     status = result.get_status()
-    if(status.eq(STATUS.NODE_CONFIGURED)):
+    if status.eq(STATUS.NODE_CONFIGURED):
         node_configured = True
         print ("<<< '%s' is configured on the Controller" % vr_cfg_params['nodeName_1'])
-    elif(status.eq(STATUS.DATA_NOT_FOUND)):
+    elif status.eq(STATUS.DATA_NOT_FOUND):
         node_configured = False
     else:
-        print ("\n")
-        print "Failed to get configuration status for the '%s'" % vr_cfg_params['nodeName_1']
-        print ("!!!Demo terminated, reason: %s" % status.detailed())
+        logging.info ("\n")
+        logging.info ("Failed to get configuration status for the '%s'" % vr_cfg_params['nodeName_1'])
+        logging.info ("!!!Demo terminated, reason: %s" % status.detailed())
         exit(0)
 
     if node_configured is False:
         result = ctrl.add_netconf_node(vrouter)
         status = result.get_status()
-        if(status.eq(STATUS.OK)):
-            print ("<<< '%s' added to the Controller" % vr_cfg_params['nodeName_1'])
+        if status.eq(STATUS.OK):
+            logging.info ("<<< '%s' added to the Controller" % vr_cfg_params['nodeName_1'])
         else:
-            print ("\n")
-            print ("!!!Demo terminated, reason: %s" % status.detailed())
+            logging.info ("\n")
+            logging.info ("!!!Demo terminated, reason: %s" % status.detailed())
             exit(0)
 
     result = ctrl.check_node_conn_status(vr_cfg_params['nodeName_1'])
     status = result.get_status()
-    if(status.eq(STATUS.NODE_CONNECTED)):
-        print ("<<< '%s' is connected to the Controller" % vr_cfg_params['nodeName_1'])
+    if status.eq(STATUS.NODE_CONNECTED):
+        logging.info ("<<< '%s' is connected to the Controller" % vr_cfg_params['nodeName_1'])
     else:
-        print ("\n")
-        print ("!!!Demo terminated, reason: %s" % status.brief().lower())
+        logging.info ("\n")
+        logging.info ("!!!Demo terminated, reason: %s" % status.brief().lower())
         exit(0)
+
+    set_loopback_interface_1(ctrl, vr_cfg_params)
 
     dp1_1 = DataPlaneInterface(vr_cfg_params['dp1Name_1'])
     dp1_1.set_address(vr_cfg_params['dp1Addr_1'])
     vrouter.set_dataplane_interface_cfg(dp1_1)
-
-    set_ospf_param(ctrl, vr_cfg_params)
-
 
     dp2_1 = DataPlaneInterface(vr_cfg_params['dp1Name_2'])
     dp2_1.set_address(vr_cfg_params['dp1Addr_2'])
@@ -189,10 +220,7 @@ def do_vr_cfg_1(vr_cfg_params):
     dp3_1.set_address(vr_cfg_params['dp1Addr_3'])
     vrouter.set_dataplane_interface_cfg(dp3_1)
 
-    ospf = Ospf()
-    ospf.set_ospf_parameters(vr_cfg_params['ospfParams-RouterId_1'])
-    ospf.set_ospf_area(vr_cfg_params['ospfArea-0000-1_1'])
-    ospf.set_ospf_area(vr_cfg_params['ospfArea-0000-2_1'])
+    set_ospf_cfg_1(ctrl, vr_cfg_params)
 
     sys.exit(0)
 
@@ -201,11 +229,54 @@ def do_vr_cfg_2(vr_cfg_params):
 
     ctrl = Controller(vr_cfg_params['ctrlIpAddr'], vr_cfg_params['ctrlPortNum'],
                       vr_cfg_params['ctrlUname'], vr_cfg_params['ctrlPswd'])
+
     vrouter = VRouter5600(ctrl, vr_cfg_params['nodeName_2'], vr_cfg_params['nodeIpAddr_2'],
                           vr_cfg_params['nodePortNum_2'], vr_cfg_params['nodeUname_2'], vr_cfg_params['nodePswd_2'])
-    mount_vr(ctrl, vrouter, vr_cfg_params['nodeName_2'])
-    print ("<<< 'Controller': %s, '%s': %s"
-           % (vr_cfg_params['ctrlIpAddr'], vr_cfg_params['nodeName_2'], vr_cfg_params['nodeIpAddr_2']))
+
+    node_configured = False
+    result = ctrl.check_node_config_status(vr_cfg_params['nodeName_2'])
+    status = result.get_status()
+    if status.eq(STATUS.NODE_CONFIGURED):
+        node_configured = True
+        logging.info ("<<< '%s' is configured on the Controller" % vr_cfg_params['nodeName_2'])
+    elif status.eq(STATUS.DATA_NOT_FOUND):
+        node_configured = False
+    else:
+        logging.info ("\n")
+        logging.info ("Failed to get configuration status for the '%s'" % vr_cfg_params['nodeName_2'])
+        logging.info ("!!!Demo terminated, reason: %s" % status.detailed())
+        exit(0)
+
+    if node_configured is False:
+        result = ctrl.add_netconf_node(vrouter)
+        status = result.get_status()
+        if status.eq(STATUS.OK):
+            logging.info ("<<< '%s' added to the Controller" % vr_cfg_params['nodeName_2'])
+        else:
+            logging.info ("\n")
+            logging.info ("!!!Demo terminated, reason: %s" % status.detailed())
+            exit(0)
+
+    result = ctrl.check_node_conn_status(vr_cfg_params['nodeName_1'])
+    status = result.get_status()
+    if status.eq(STATUS.NODE_CONNECTED):
+        logging.info ("<<< '%s' is connected to the Controller" % vr_cfg_params['nodeName_2'])
+    else:
+        logging.info ("\n")
+        logging.info ("!!!Demo terminated, reason: %s" % status.brief().lower())
+        exit(0)
+
+    set_loopback_interface_2(ctrl, vr_cfg_params)
+
+    dp1_2 = DataPlaneInterface(vr_cfg_params['dp1Name_2'])
+    dp1_2.set_address(vr_cfg_params['dp1Addr_2'])
+    vrouter.set_dataplane_interface_cfg(dp1_2)
+
+    dp2_2 = DataPlaneInterface(vr_cfg_params['dp2Name_2'])
+    dp2_2.set_address(vr_cfg_params['dp2Addr_2'])
+    vrouter.set_dataplane_interface_cfg(dp2_2)
+
+    set_ospf_cfg_2(ctrl, vr_cfg_params)
 
     sys.exit(0)
 
@@ -216,9 +287,8 @@ def do_vr_cfg_3(vr_cfg_params):
                       vr_cfg_params['ctrlUname'], vr_cfg_params['ctrlPswd'])
     vrouter = VRouter5600(ctrl, vr_cfg_params['nodeName_3'], vr_cfg_params['nodeIpAddr_3'],
                           vr_cfg_params['nodePortNum_3'], vr_cfg_params['nodeUname_3'], vr_cfg_params['nodePswd_3'])
-    mount_vr(ctrl, vrouter, vr_cfg_params['nodeName_3'])
 
-    print ("<<< 'Controller': %s, '%s': %s"
+    logging.info ("<<< 'Controller': %s, '%s': %s"
            % (vr_cfg_params['ctrlIpAddr'], vr_cfg_params['nodeName_3'], vr_cfg_params['nodeIpAddr_3']))
 
     sys.exit(0)
@@ -238,7 +308,6 @@ if __name__ == "__main__":
 
     doo = read_in_vr_cfg_file()
 
-    args.vr = doo['nodeName_1']
 
     if args.vr == doo['nodeName_1']:
         do_vr_cfg_1(doo)
